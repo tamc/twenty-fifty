558d2f64064310503249a1a23d4b8751d2abd473
tamc <tamc@greenonblack.com>
2010-07-27 10:35:36 +0100

    Updated document urls

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,2 +1,12 @@
 class DocumentController < ApplicationController
+  
+  def call_for_evidence
+    redirect_to 'http://www.decc.gov.uk/en/content/cms/what_we_do/lc_uk/2050/2050.aspx'
+  end
+  
+  alias call_for_evidence_spreadsheet call_for_evidence
+  
+  def get_source_code
+    redirect_to 'http://github.com/tamc/twenty-fifty'
+  end
 end
