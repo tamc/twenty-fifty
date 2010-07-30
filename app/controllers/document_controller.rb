f51237a6bd0f32a9c0a5a6bba21b4e3aabfe375b
tamc <tamc@greenonblack.com>
2010-07-30 13:31:38 +0100

    Changed the urls used for the call for evidence

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,10 +1,16 @@
 class DocumentController < ApplicationController
   
-  def call_for_evidence
-    redirect_to 'http://www.decc.gov.uk/en/content/cms/what_we_do/lc_uk/2050/2050.aspx'
+  def call_for_evidence_response
+    redirect_to 'http://econsultation.decc.gov.uk/decc-executive/2050_pathways/consult_view'
   end
   
-  alias call_for_evidence_spreadsheet call_for_evidence
+  def call_for_evidence
+    redirect_to 'http://www.decc.gov.uk/media/viewfile.ashx?filetype=4&filepath=What%20we%20do/A%20low%20carbon%20UK/2050/216-2050-pathways-analysis-report.pdf&minwidth=true'
+  end
+  
+  def call_for_evidence_spreadsheet
+    redirect_to 'http://www.decc.gov.uk/media/viewfile.ashx?filetype=4&filepath=What%20we%20do/A%20low%20carbon%20UK/204-2050-calculator-spreadsheet.xlsx&minwidth=true'
+  end
   
   def get_source_code
     redirect_to 'http://github.com/tamc/twenty-fifty'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ ActionController::Routing::Routes.draw do |map|
     pathways.resources :sectors, :member => {:primary_energy_chart => :get, :check_if_calculated => :get}
   end
   map.resources :sectors, :member => {:primary_energy_chart => :get, :check_if_calculated => :get}
-  map.resources :document, :collection => {:help => :get, :get_source_code => :get, :call_for_evidence_spreadsheet => :get, :call_for_evidence => :get, :make_it_better => :get}
+  map.resources :document, :collection => {:help => :get, :get_source_code => :get, :call_for_evidence_spreadsheet => :get, :call_for_evidence => :get, :make_it_better => :get, :call_for_evidence_response => :get}
   map.root :controller => 'document', :action => 'index'
 end
