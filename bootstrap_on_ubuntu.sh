887bf3b97ad35eae7200bb460bd315557b13c611
Tom Counsell <tamc@greenonblack.com>
2010-08-18 07:49:05 -0700

    Removed the (now obsolete) check for the vendored gems directory

--- bootstrap_on_ubuntu.sh
+++ bootstrap_on_ubuntu.sh
@@ -3,11 +3,6 @@
 # This script downloads the required libraries
 # and then starts the calculator
 
-if [ ! -f vendor/gems ]
-then
-    echo "Please run this script in the 2050 application directory"
-    exit 1
-fi
 sudo apt-get install ruby1.9.1-full rubygems1.9.1 sqlite3 libsqlite3-dev
 sed -i "s/'ruby'/'ruby1.9.1'/" setup.sh run.sh
 sed -i "s/'rake'/'rake1.9.1'/" setup.sh run.sh
